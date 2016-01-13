41e4185d80eeb9582d472e4d8d610efb72773c1c
Loïc Nogues <nogues.loic@gmail.com>
2016-01-13 14:24:45 +0100

    README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Node-libRAW
+
+How to use:
+
+```
+var libraw = require('node-libraw');
+
+var outputFile = libraw('./test.raf', './output');
+```
--- libraw.js
+++ libraw.js
@@ -1,6 +1,3 @@
 var libraw = require('./build/Release/raw');
 
-//console.log(libraw.hello('world'));
-//console.log(libraw.extractThumb('./test.raf', './extracted'));
-
 module.exports = libraw;
