ba827413d02116c4cffe94b45dc34820da59f331
Loïc Nogues <nogues.loic@gmail.com>
2016-01-13 14:22:39 +0100

    Remove console.log

--- libraw.js
+++ libraw.js
@@ -1,5 +1,6 @@
 var libraw = require('./build/Release/raw');
 
-console.log(libraw.hello('world'));
-//console.log(libraw.openFile('./test.raf'));
-console.log(libraw.extractThumb('./test.raf', './extracted'));
+//console.log(libraw.hello('world'));
+//console.log(libraw.extractThumb('./test.raf', './extracted'));
+
+module.exports = libraw;
--- node_libraw.cc
+++ node_libraw.cc
@@ -19,7 +19,7 @@ namespace node_libraw {
   using v8::Value;
   using v8::Null;
 
-  using Nan::Callback;
+  //using Nan::Callback;
 
   NAN_METHOD(Hello) {
     Nan::HandleScope scope;
@@ -33,8 +33,6 @@ namespace node_libraw {
 
   NAN_METHOD(ExtractThumb) {
     Nan::HandleScope scope;
-
-    int ret = 0;
 
     LibRaw RawProcessor;
 
@@ -53,118 +51,23 @@ namespace node_libraw {
     std::vector<char> buffer(size);
 
     if (file.read(buffer.data(), size)) {
-      std::cout << size << "\n";
-      ret = RawProcessor.open_buffer(buffer.data(), size);
-      ret = RawProcessor.unpack();
-      ret = RawProcessor.unpack_thumb();
-      //std::string extension = RawProcessor.imgdata.thumbnail == LIBRAW_THUMBNAIL_JPEG ? "thumb.jpg" : "thumb.ppm";
+      RawProcessor.open_buffer(buffer.data(), size);
+      RawProcessor.unpack();
+      RawProcessor.unpack_thumb();
 
       if (RawProcessor.imgdata.thumbnail.tformat == LIBRAW_THUMBNAIL_JPEG) {
         extension = "thumb.jpg";
       }
 
       output = output + "." + extension;
-      ret = RawProcessor.dcraw_thumb_writer(output.c_str());
+      RawProcessor.dcraw_thumb_writer(output.c_str());
       RawProcessor.recycle();
 
-      std::string returnValue = "error";
-
-      if (ret == LIBRAW_SUCCESS) {
-        returnValue = "success";
-      }
-
-      info.GetReturnValue().Set(Nan::New(returnValue).ToLocalChecked());
+      info.GetReturnValue().Set(Nan::New(output).ToLocalChecked());
     }
 
     file.close();
   }
-
-//  NAN_METHOD(OpenFile) {
-//    Isolate* isolate = info.GetIsolate();
-//    const unsigned argc = 1;
-
-//    int ret = 0;
-//    //char thumbfn[1024];
-
-//    std::string thumbfn = "";
-
-//    Nan::HandleScope scope;
-
-//    // Creation of image processing object
-//    LibRaw RawProcessor;
-
-//#define P1  RawProcessor.imgdata.idata
-//#define S   RawProcessor.imgdata.sizes
-//#define C   RawProcessor.imgdata.color
-//#define T   RawProcessor.imgdata.thumbnail
-//#define P2  RawProcessor.imgdata.other
-//#define OUT RawProcessor.imgdata.params
-
-//    Callback *callback = new Callback(info[1].As<Function>());
-//    libraw_processed_image_t *image = NULL;
-
-//    OUT.output_tiff = 1; // Let us output TIFF
-
-//    v8::string::utf8value namefromargs(info[0]->tostring());
-//    std::string filename = std::string(*namefromargs);
-
-//    std::ifstream file;
-//    file.open(filename, std::ios::binary | std::ios::ate);
-//    std::streamsize size = file.tellg();
-//    file.seekg(0, std::ios::beg);
-
-//    std::vector<char> buffer(size);
-
-//    if (file.read(buffer.data(), size)) {
-//      std::cout << size << "\n";
-//      ret = rawprocessor.open_buffer(buffer.data(), size);
-
-//      if(ret != LIBRAW_SUCCESS) {
-//        RawProcessor.recycle(); 
-//      }
-
-//      // Let us unpack the image
-//      if( (ret = RawProcessor.unpack() ) != LIBRAW_SUCCESS) {
-//        //fprintf(stderr,"Cannot unpack_thumb %s\n", filename);
-//        std::cout << "Cannot unpack " << filename << "\n";
-//      }
-
-//      if( (ret = RawProcessor.unpack_thumb() ) != LIBRAW_SUCCESS){
-//        std::cout << "Cannot unpack_thumb " << filename << "\n";
-//      }
-//      else {
-//        //snprintf(thumbfn, sizeof(thumbfn), "%s.%s", filename, T.tformat == LIBRAW_THUMBNAIL_JPEG ? "thumb.jpg" : "thumb.ppm");
-//        thumbfn = "test.jpg";
-
-//        if( LIBRAW_SUCCESS != (ret = RawProcessor.dcraw_thumb_writer(thumbfn.c_str()))){
-//          std::cout << "Cannot write " << thumbfn << "\n";
-//        }
-//      }
-
-//      // Data unpacking
-//      ret = RawProcessor.dcraw_process();
-//      image = RawProcessor.dcraw_make_mem_image(&ret);
-
-////#define SWAP(a,b) { a ^= b; a ^= (b ^= a); }
-////      if (image->bits == 16 && htons(0x55aa) != 0x55aa)
-////        for(unsigned i=0; i< image->data_size; i+=2)
-////          SWAP(image->data[i],image->data[i+1]);
-////#undef SWAP
-
-//      //Local<Value> emitArgs[1];
-//      //emitArgs[0] = Nan::CopyBuffer((char*) image->data, image->data_size);
-
-//      //Nan::MaybeLocal<Value> argv[argc] = { 
-//      //  Nan::CopyBuffer((char*) image->data, image->data_size) 
-//      //};
-
-//      //callback->Call(Null(isolate), argc, argv);
-
-//      return Nan::CopyBuffer((char*) image->data, image->data_size);
-//    }
-
-//    file.close();
-//  }
 
   void init(Local<Object> exports) {
     Nan::Set(
@@ -172,12 +75,6 @@ namespace node_libraw {
       Nan::New<String>("hello").ToLocalChecked(),
       Nan::GetFunction(Nan::New<v8::FunctionTemplate>(Hello)).ToLocalChecked()
     );
-
-    //Nan::Set(
-    //  exports,
-    //  Nan::New<String>("openFile").ToLocalChecked(),
-    //  Nan::GetFunction(Nan::New<v8::FunctionTemplate>(OpenFile)).ToLocalChecked()
-    //);
 
     Nan::Set(
       exports,
