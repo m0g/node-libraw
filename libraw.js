e7b723cca6ad4338bb93c382bada88dd0d29f510
Loïc Nogues <nogues.loic@gmail.com>
2016-04-07 12:34:47 +0200

    Test if file exists before attempting to extract it

--- libraw.js
+++ libraw.js
@@ -1,24 +1,38 @@
+const fs = require('fs');
+
 const raw = require('./build/Release/node_libraw');
 
 const libraw = {
   extract: function(input, output) {
     return new Promise(function(resolve, reject) {
-      raw.extract(input, output, function(err, output) {
+      fs.access(input, fs.R_OK | fs.W_OK, function(err) {
         if (err)
           reject(err);
+
         else
-          resolve(output);
+          raw.extract(input, output, function(err, output) {
+            if (err)
+              reject(err);
+            else
+              resolve(output);
+          });
       });
     });
   },
 
   extractThumb: function(input, output) {
     return new Promise(function(resolve, reject) {
-      raw.extractThumb(input, output, function(err, output) {
+      fs.access(input, fs.R_OK | fs.W_OK, function(err) {
         if (err)
           reject(err);
+
         else
-          resolve(output);
+          raw.extractThumb(input, output, function(err, output) {
+            if (err)
+              reject(err);
+            else
+              resolve(output);
+          });
       });
     });
   }
